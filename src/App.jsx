0e55bad41cd185a14a2e3f3736e6255cf5682917
yousef kassab <yousefkassabw.99@gmail.com>
2023-07-01 19:08:09 +0300

    movix

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ function App() {
     useEffect(() => {
         fetchApiConfig();
         genresCall();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const fetchApiConfig = () => {
