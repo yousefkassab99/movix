58ec89e79672a54cb7dc498c17e66de1ae671e32
yousef kassab <yousefkassabw.99@gmail.com>
2023-07-01 18:57:02 +0300

    movix

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,23 @@
 import axios from "axios";
 
+const TOKEN =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYTQ2NzM2NDk2MTZjYmExMDg2MDU5MWU2YWU0NTcyNCIsInN1YiI6IjY0OTQ1OTQzZTIyZDI4MDBhZTk1ZDBmOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-l_6Myl4q67GNCUv7QfIObhTu_k82oxVmvzoqtHnORo";
 const BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+const TMDB_TOKEN = TOKEN;
 
 const headers = {
-    Authorization : "bearer" + TMDB_TOKEN,
+  Authorization: "bearer " + TMDB_TOKEN,
 };
 
-
-export const fetchDataFromApi = async(url, params) => {
-try {
-    const {data} = await axios.get(BASE_URL + url, {
-        headers,
-        params
-    })
+export const fetchDataFromApi = async (url, params) => {
+  try {
+    const { data } = await axios.get(BASE_URL + url, {
+      headers,
+      params,
+    });
     return data;
-} catch (err) {
+  } catch (err) {
     console.log(err);
     return err;
-}
-}
+  }
+};
